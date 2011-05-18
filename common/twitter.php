b35d2a3fce434d52b4dc9829fa57fa57ba6f59b8
terence.eden@gmail.com <terence.eden@gmail.com@760c5f7a-8625-11dd-9205-735403a6da14>
2011-05-18 15:59:45 +0000

    Fixes bug identified in r401 by Embedly. Now sends a User-Agent on all requests.  By default this is "Mozilla/5.0 (compatible; dabr; BASE_URL)" where BASE_URL is the name of your server EG http://example.com/dabr
    git-svn-id: http://dabr.googlecode.com/svn/trunk@402 760c5f7a-8625-11dd-9205-735403a6da14

--- common/twitter.php
+++ common/twitter.php
@@ -485,6 +485,8 @@ function twitter_fetch($url) {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	//curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	$user_agent = "Mozilla/5.0 (compatible; dabr; " . BASE_URL . ")";
+	curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$response = curl_exec($ch);
 	curl_close($ch);
